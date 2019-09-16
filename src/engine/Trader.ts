56a397970c30c11fa20025be8a5da9b724487c06
Dmitry Fokin <fokind1@yandex.ru>
2019-09-16 22:43:30 +0300

    TraderEngine

--- src/engine/Trader.ts
+++ src/engine/Trader.ts
@@ -1,4 +1,4 @@
-import EventEmitter from "events";
+// import EventEmitter from "events";
 import { ObjectID } from "mongodb";
 import { Worker } from "worker_threads";
 import connect from "../connect";
@@ -7,9 +7,42 @@ import { Trader } from "../models/Trader";
 const collectionName = "trader";
 const marketWorkers: any = {};
 
-export class TraderEngine extends EventEmitter {
-  public static async onNewCandle(event: any) {
+export class TraderEngine {
+  public static async onAdvice(event: any): Promise<void> {
     console.log(event);
+    // купить или продать
+  }
+
+  public static async getAdvice(event: any): Promise<number> {
+    console.log(event);
+    return 1;
+  }
+
+  public static async onCandle(event: any): Promise<void> {
+    console.log(event);
+    // TODO выполнить расчет индикаторов
+    // если есть сигнал, то выполнить что дальше, использовать события нет смсыла, т.к. цепочка заданная
+    const currentAdvice: number = 0; // получить текущий, видимо из Trader
+    const advice = await TraderEngine.getAdvice(event);
+    if (advice !== currentAdvice) {
+      await TraderEngine.onAdvice(event);
+    }
+  }
+
+  public static async startMarket(options: {
+    exchange: string;
+    currency: string;
+    asset: string;
+    period: number;
+  }): Promise<void> {
+    return new Promise(resolve => {
+      const marketWorker = new Worker("./lib/worker/market.js", {
+        workerData: options
+      });
+      marketWorker.once("online", resolve);
+      marketWorker.on("message", TraderEngine.onCandle);
+      marketWorkers[JSON.stringify(options)] = marketWorker;
+    });
   }
 
   public static async start(key: string): Promise<void> {
@@ -19,15 +52,22 @@ export class TraderEngine extends EventEmitter {
     const { exchange, currency, asset, period } = new Trader(
       await db.collection(collectionName).findOne({ _id })
     );
+    return TraderEngine.startMarket({ exchange, currency, asset, period });
+  }
+
+  public static async stopMarket(options: {
+    exchange: string;
+    currency: string;
+    asset: string;
+    period: number;
+  }): Promise<void> {
     return new Promise(resolve => {
-      const marketWorker = new Worker("./lib/worker/market.js", {
-        workerData: { exchange, currency, asset, period }
-      });
-      marketWorker.once("online", resolve);
-      marketWorker.on("message", TraderEngine.onNewCandle);
-      marketWorkers[
-        JSON.stringify({ exchange, currency, asset, period })
-      ] = marketWorker;
+      const marketWorkerKey = JSON.stringify(options);
+      const marketWorker = marketWorkers[marketWorkerKey];
+      marketWorker.once("exit", resolve);
+      marketWorker.off("message", TraderEngine.onCandle);
+      delete marketWorkers[marketWorkerKey];
+      marketWorker.terminate();
     });
   }
 
@@ -38,31 +78,6 @@ export class TraderEngine extends EventEmitter {
     const { exchange, currency, asset, period } = new Trader(
       await db.collection(collectionName).findOne({ _id })
     );
-
-    return new Promise(resolve => {
-      const marketWorkerKey = JSON.stringify({
-        exchange,
-        currency,
-        asset,
-        period
-      });
-      const marketWorker = marketWorkers[marketWorkerKey];
-      marketWorker.once("exit", resolve);
-      marketWorker.off("message", TraderEngine.onNewCandle);
-      delete marketWorkers[marketWorkerKey];
-      marketWorker.terminate();
-    });
-  }
-
-  public key: string;
-  public asset: string;
-  public currency: string;
-  public exchange: string;
-  public period: number;
-  public newCandle: Event;
-
-  constructor(data: any) {
-    super();
-    Object.assign(this, data);
+    return TraderEngine.stopMarket({ exchange, currency, asset, period });
   }
 }
